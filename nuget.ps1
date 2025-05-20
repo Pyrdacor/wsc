e985a086b6a4c0bc14fcce4f7246460b8601ddea
Pyrdacor <trobt@web.de>
2025-05-21 01:04:32 +0200

    Argh

--- nuget.ps1
+++ nuget.ps1
@@ -4,4 +4,5 @@ if ($isWindows) {
   Write-Host Copy nuget packages
   mkdir "nuget"
   xcopy /Y /I "PYC.Compression\bin\Any CPU\Release\*.nupkg" "nuget\*"
+  xcopy /Y /I "PYC.Core\bin\Any CPU\Release\*.nupkg" "nuget\*"
 }
